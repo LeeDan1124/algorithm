69dfccdb5c27aa78de62783ac1f9f59ddfbab8de
lidan <dli25@trip.com>
2024-04-03 19:23:10 +0800

    update

--- 算法/滑动窗口/438. ?找到字符串中所有字母异位词.js
+++ 算法/滑动窗口/438. ?找到字符串中所有字母异位词.js
@@ -5,6 +5,7 @@
 示例 1:
 
 输入: s = "cbaebabacd", p = "abc"
+'fawrsacba'
 输出: [0,6]
 解释:
 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
@@ -17,3 +18,65 @@
 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。 */
+
+function getStr(s, p) {
+
+  if (!s || !p || typeof s !== 'string' || typeof p !== 'string') return []
+
+  const result = []
+
+  const targetMap = p.split('').reduce((pre, cur) => {
+    if (typeof pre[cur] !== 'number') {
+      pre[cur] = 0
+    }
+    pre[cur] = pre[cur] + 1
+    return pre
+  }, {})
+  const targetNumber = Object.keys(targetMap).length
+
+  let left = 0, right = 0, curObj = {}, curNumber = 0
+
+  while(right < s.length) {
+
+    const r = s[right]
+    if (p.includes(r)) {
+
+      if (typeof curObj[r] !== 'number') {
+        curObj[r] = 0
+      }
+      curObj[r] = curObj[r] + 1
+
+      if (curObj[r] === targetMap[r]) {
+        curNumber++
+      }
+
+      // TODO 当超过之后也要做处理
+      // if (curObj[r] > targetMap[r]) {
+        
+      // }
+
+      while(curNumber === targetNumber) {
+        result.push(left)
+        const ls = s[left]
+        if (curObj[ls] === targetMap[ls]) {
+          curObj[ls] = curObj[ls] - 1
+          curNumber--
+        }
+        left++
+      }
+      right++
+
+    } else {
+      right++
+      left = right
+      curObj = {}
+      curNumber = 0
+    }
+  }
+
+  return result
+}
+
+console.log(getStr('cbaebabacd', 'abc'))
+// console.log(getStr('fawrsacba', 'abc'))
+// console.log(getStr('abab', 'ab'))
